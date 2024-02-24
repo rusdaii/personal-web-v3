664ddd6931b0bae771dd6943762082ffba7cf4a7
rusdaii <rusdaii.html@gmail.com>
2024-02-24 22:43:20 +0700

    fix: add target _blank to project button

--- src/components/parts/Project/ProjectLink/index.tsx
+++ src/components/parts/Project/ProjectLink/index.tsx
@@ -17,14 +17,18 @@ const ProjectLink = ({ className, github, demo }: ProjectLinkProps) => {
       >
         <Link
           href={github}
+          target="_blank"
           className="flex justify-center items-center gap-2 hover:scale-110 transition-all duration-300"
+          aria-label="Project Github"
         >
           <FaGithub />
           <span className="text-base">Github</span>
         </Link>
         <Link
           href={demo}
+          target="_blank"
           className="flex justify-center items-center gap-2 hover:scale-110 transition-all duration-300"
+          aria-label="Project Demo Site"
         >
           <FaExternalLinkAlt />
           <span className="text-base">Demo</span>
